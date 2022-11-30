361f9a8ed98d03d39c6b2fd1a8ac66f1179e5b22
DESKTOP-5VLRRKB\wlrmw <wlrmworms@gmail.com>
2022-11-30 14:23:41 +0900

    no message

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	//기본 출력
 	fmt.Fprintf(w, "Golang WebServer Working!")
-	fmt.Println("재근이1")
+	fmt.Println("재근이12")
 }
 
 func main() {
